b4d43198d542f2ee7cb8a503afb0c1068de1c134
Alexandria Cowan <alexandriacowan@Alexs-MacBook-Pro.local>
2019-07-29 14:55:08 -0700

    update add restaurant

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var button = $('button');
 var result =$('.result');
 
+var addNewButton = $('.addnew');
+
 
 var restaurants = ["Annapurna", "Taco Bell", "Gyros", "Aviv Hummus Bar", "Plum", "Kati Thai", "Araya's Place", "No Bones Beach Club", "Highline", "El Borracho", "Dino's Pizza"];
 
@@ -12,4 +14,14 @@ function randomizeResult() {
   var restaurant = restaurants[randomWholeNumber];
   // console.log(restaurant);
   result.text(`You should eat at ${restaurant}!`)
+}
+
+
+addNewButton.on("click", addRestaurant)
+
+function addRestaurant() {
+  var newRestaurant = $('input').val();
+  restaurants.push(newRestaurant);
+  console.log(restaurants);
+  $('input').val("");
 }
